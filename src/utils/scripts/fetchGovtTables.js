898c89901342fdd0eccd8f4eeb7b58f4d5ecc1e8
Dalmo Mendonça <dalmo3@gmail.com>
2020-04-09 16:25:52 +1200

    add case dara from 9 apr

--- src/utils/scripts/fetchGovtTables.js
+++ src/utils/scripts/fetchGovtTables.js
@@ -74,9 +74,11 @@ document.querySelectorAll('table.table-style-two').forEach(table => {
       .sort((a, b) => (a.gender < b.gender ? 1 : -1))
       .sort((a, b) => (a.dhb < b.dhb ? 1 : -1))
       .sort((a, b) => {
-        let aDate = a.report_date.split('/');
-        let bDate = b.report_date.split('/');
-        return aDate[1] + aDate[0] - (bDate[1] + bDate[0]);
+        let aDateArr = a.report_date.split('/');
+        let bDateArr = b.report_date.split('/');
+        let aDate = new Date(aDateArr[2] + '-' + aDateArr[1] + '-' + aDateArr[0])
+        let bDate = new Date(bDateArr[2] + '-' + bDateArr[1] + '-' + bDateArr[0])
+        return aDate.getTime() - bDate.getTime();
       });
       break;
       default:
